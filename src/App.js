a45a0a6d1c77d14c37c82468e7c691ad5a8d1246
chrisclarkedev <cclarke8904@gmail.com>
2021-02-10 21:20:59 -0500

    fetched images from api

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Movie from "./components/Movie";
 
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?/sort_by=popularity.desc&api_key=${process.env.REACT_APP_UNSPLASH_KEY}&page=1;`;
 
-// const IMG_API = "https://image.tmdb.org/t/p/w1280";
-
 // const SEARCH_API =
 //   "https://api.themoviedb.org/3/search/movie?&api_key=${process.env.REACT_APP_UNSPLASH_KEY}&query=";
 
@@ -17,10 +15,15 @@ function App() {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        setMovies(data);
+        setMovies(data.results);
       });
   }, []);
-  return <div>{movies.length > 0 && movies.map((movie) => <Movie />)}</div>;
+  return (
+    <div>
+      {movies.length > 0 &&
+        movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,10 @@
 import React from "react";
 
-const Movie = () => (
-  <div className="movie">This is where the movie component is</div>
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
+const Movie = ({ title, poster_path, overview, vote_average }) => (
+  <div className="movie">
+    <img src={IMG_API + poster_path} alt={title} />
+  </div>
 );
 export default Movie;
