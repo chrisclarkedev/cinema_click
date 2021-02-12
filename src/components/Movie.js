ca798db84237a88c9ed82508189ad1fc0efb8b13
chrisclarkedev <cclarke8904@gmail.com>
2021-02-11 21:20:25 -0500

    overview of movie added, minor style changes

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -9,6 +9,10 @@ const Movie = ({ title, poster_path, overview, vote_average }) => (
       <h3>{title}</h3>
       <span>{vote_average}</span>
     </div>
+    <div className="movie-overview">
+      <h2>Overview:</h2>
+      <p>{overview}</p>
+    </div>
   </div>
 );
 export default Movie;
