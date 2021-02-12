0a9f675ee22be08c2427df7c192ddc0c8d0e012b
chrisclarkedev <cclarke8904@gmail.com>
2021-02-11 21:47:11 -0500

    added search bar to top of page

--- src/App.js
+++ src/App.js
@@ -18,10 +18,15 @@ function App() {
       });
   }, []);
   return (
-    <div className="movie-container">
-      {movies.length > 0 &&
-        movies.map((movie) => <Movie key={movie.id} {...movie} />)}
-    </div>
+    <>
+      <header>
+        <input className="search" type="search" placeholder="Search..." />
+      </header>
+      <div className="movie-container">
+        {movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
+    </>
   );
 }
 
